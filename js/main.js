1b48a443556aae94c59d39b4ccbb901e5f4dccc2
Y2Kuwu <jamesmfox5@gmail.com>
2022-08-09 11:40:15 -0700

    still undef, may delete majority

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ const playBtn = document.getElementById("playButton");
 const retryBtn = document.getElementById("retryButton");
 const quitBtn = document.getElementById("quitButton")
 
-let totCred = document.getElementById("credCounter");
-let cred = document.getElementById("balance");
+//let totCred = document.getElementById("credCounter");
+let cred = document.querySelector(".balance");
 
 const shine = document.getElementById("star");
 const rock = document.getElementById("diamond");
@@ -18,57 +18,63 @@ const lord = document.getElementById("king");
 const lady = document.getElementById("queen");
 const jest = document.getElementById("jester");
 
-function startBal(cc , plusSub){
-    let newVal;
-    cc += plusSub = newVal;
+cred.append(5);
+let newVal = document.createElement("newVal");
+let updatedCred = cred.value + newVal;
+//let windowId = win.open("file:///Users/jamesfox/Desktop/slot_machine_project/Slot_machine1-/index.html");
+//let winClose = 
 
-    return cred.innerHTML = newVal;  //
-}
-//startBal();
 
-function customerCreditScore(cs){
+function customerCreditScore(cs){ //for 
     if(cred = 0){
         isPlaying = false;
         alert("OUT OF CREDITS");
+        
     }
     else{
         isPlaying = true;
     }
 }
 
-retryBtn.addEventListener("click", function(){
 
-});
-
-for(let play = 0; play < clicked; play++)
-{
+//three identical arrays 
 slot1= [shine , rock , coldBlood , cat , tri, scope , ingot , dagger , lord , lady , jest];
 slot2= [shine , rock , coldBlood , cat , tri, scope , ingot , dagger , lord , lady , jest];
 slot3= [shine , rock , coldBlood , cat , tri, scope , ingot , dagger , lord , lady , jest];
-slotOutput = [];
-makeSpace = [];
 
-let row1 = slot1[Math.floor(Math.random()*slot1.length)];
+makeSpace = []; //for resetting
+
+let row1 = slot1[Math.floor(Math.random()*slot1.length)]; //random res
 let row2 = slot2[Math.floor(Math.random()*slot2.length)];
 let row3 = slot3[Math.floor(Math.random()*slot3.length)];
-slotOutput = [row1,row2,row3];
+slotOutput = [row1,row2,row3]; //new array with three above 
+//
 
-const initVal = cred;
-const credArray = [1, 5, -1];
-const updateCred = [];
-}
+
+let updateCred;
+
 
 playBtn.addEventListener("click", function(){  //find a way to refresh or empty arrays
+    const credArray = [1, 5, -1];
     if(row1 == row2 && row3){ // triple
         alert("triple");
-        cred.innerHTML = initVal + credArray[2];
+        //newVal = credArray[1];
+        cred.append(cred.value + credArray[1]);
     }
     else if(row1 == row2 || row1 == row3 || row2 == row3){ //double
         alert("double");
-        cred.innerHTML = initVal + credArray[1];
+        //cred.setAttribute("value" + credArray[2]);
+        //newVal = credArray[0];
+        cred.append(cred.value + credArray[0]);
     }
     else { //none
         alert("no match");
-        cred.innerHTML = initVal + credArray[3];
+        //newVal = credArray[2];
+        cred.append(cred -1);
+        
     }
+});
+
+retryBtn.addEventListener("click", function(){
+
 });
